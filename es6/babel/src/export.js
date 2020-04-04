817165505d4f5f788df236548b30cb86bb372e51
unknown <1649495460@qq.com>
2020-04-04 19:34:04 +0800

    4.4

---
+++ es6/babel/src/export.js
@@ -0,0 +1,13 @@
+// export let a = 10;
+// export let foo = ()=>"hello";
+
+class Person{
+  constructor(name){
+    this.name = name;
+  }
+  showName(){
+    console.log(this.name);
+    
+  }
+}
+export default Person;
---
+++ es6/babel/src/import.js
@@ -0,0 +1,8 @@
+// import {a as c,foo} from "./export";
+// console.log(c);
+// console.log(foo());
+
+import Teacher from "./export";
+
+let t1 = new Teacher("小李");
+t1.showName();
